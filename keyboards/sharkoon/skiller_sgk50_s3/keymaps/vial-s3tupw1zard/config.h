65d4d2451f9cb7e348d861e160366ac73df0176d
s3tupw1zard <50152835+s3tupw1zard@users.noreply.github.com>
2025-02-16 15:44:49 +0100

    Added new custom keymap with default vial configs for SKILLER SGK50 S3

---
+++ keyboards/sharkoon/skiller_sgk50_s3/keymaps/vial-s3tupw1zard/config.h
@@ -0,0 +1,8 @@
+/* SPDX-License-Identifier: GPL-2.0-or-later */
+
+#pragma once
+
+#define VIAL_KEYBOARD_UID {0x48, 0xBE, 0xB5, 0xE0, 0x5F, 0xA9, 0x85, 0xE0}
+
+#define VIAL_UNLOCK_COMBO_ROWS { 0, 3 }
+#define VIAL_UNLOCK_COMBO_COLS { 0, 13 }
